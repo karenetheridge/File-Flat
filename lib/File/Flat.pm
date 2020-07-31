952ec6e5a6a52882ece6175046da546dfda2ad1e
Karen Etheridge <ether@cpan.org>
2020-07-31 09:06:12 -0700

    increment $VERSION after 1.06 release

--- lib/File/Flat.pm
+++ lib/File/Flat.pm
@@ -17,7 +17,7 @@ use prefork    'File::Copy';
 use prefork    'File::Copy::Recursive';
 use prefork    'File::Remove';
 
-our $VERSION = '1.06';
+our $VERSION = '1.07';
 
 # The main error string
 our $errstr  = '';
@@ -514,7 +514,7 @@ sub _andRemove {
 
 
 package File::Flat::Object;
-our $VERSION = '1.06';
+our $VERSION = '1.07';
 
 # Instantiatable version of File::Flat.
 # 
